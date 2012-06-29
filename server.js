50e708c6222a4741cc4b6f7b02f1dd4f6678a4b2
root <root@betbot.(none)>
2012-06-29 17:36:06 +0000

    my super message

--- server.js
+++ server.js
@@ -1,6 +1,106 @@
+/**
+ * Quote Stream
+ *
+ * @version 0.2.1
+ * @author NodeSocket <http://www.nodesocket.com> <hello@nodesocket.com>
+ */
+
+ /*
+ * Copyright (C) 2012 NodeSocket LLC 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including 
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial 
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+////
+// CONFIGURATION SETTINGS
+///
+var PORT = 4000;
+var FETCH_INTERVAL = 5000;
+var PRETTY_PRINT_JSON = true;
+
+///
+// START OF APPLICATION
+///
+var express = require('express');
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node.js\n');
-}).listen(8124, "rack.sharpcode.biz");
-console.log('Server running at  http://rack.sharpcode.biz:8124/');
+var io = require('socket.io');
+
+var app = express();
+var server = http.createServer(app);
+var io = io.listen(server);
+io.set('log level', 1);
+
+server.listen(PORT);
+
+var ticker = "";
+app.get('/:ticker', function(req, res) {
+	ticker = req.params.ticker;
+	res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket) {
+	var local_ticker = ticker;
+	ticker = "";
+
+	//Run the first time immediately
+	get_quote(socket, local_ticker);
+
+	//Every N seconds
+	var timer = setInterval(function() {
+		get_quote(socket, local_ticker)
+	}, FETCH_INTERVAL);
+
+	socket.on('disconnect', function () {
+		clearInterval(timer);
+	});
+});
+
+function get_quote(p_socket, p_ticker) {
+	http.get({
+		host: 'www.google.com',
+		port: 80,
+		path: '/finance/info?client=ig&q=' + p_ticker
+	}, function(response) {
+		response.setEncoding('utf8');
+		var data = "";
+
+		response.on('data', function(chunk) {
+			data += chunk;
+		});
+
+		response.on('end', function() {
+			if(data.length > 0) {
+				try {
+					var data_object = JSON.parse(data.substring(3));
+				} catch(e) {
+					return;
+				}
+
+				var quote = {};
+				quote.ticker = data_object[0].t;
+				quote.exchange = data_object[0].e;
+				quote.price = data_object[0].l_cur;
+				quote.change = data_object[0].c;
+				quote.change_percent = data_object[0].cp;
+				quote.last_trade_time = data_object[0].lt;
+				quote.dividend = data_object[0].div;
+				quote.yield = data_object[0].yld;
+
+				p_socket.emit('quote', PRETTY_PRINT_JSON ? JSON.stringify(quote, true, '\t') : JSON.stringify(quote));
+			}
+		});
+	});
+}
