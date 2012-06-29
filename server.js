6f82981641b569175a65e2b83596872a1478d96f
root <root@betbot.(none)>
2012-06-29 16:19:49 +0000

    Second test commit

---
+++ server.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node.js\n');
+}).listen(8124, "rack.sharpcode.biz");
+console.log('Server running at  http://rack.sharpcode.biz:8124/');
