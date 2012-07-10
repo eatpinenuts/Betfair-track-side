d936e9247a5316d26870d4f0c110e0dfc3cf7845
Billy Flatman <billyflatman@Billy-Flatmans-MacBook-Pro.local>
2012-07-10 20:42:44 +0100

    Getting horses and prices

--- BFLoginAsync.js
+++ BFLoginAsync.js
@@ -9,7 +9,7 @@ var password = myArgs[1];
 var events = myArgs[2];
 var session = '';
 
-async.series([ login, getAllMarkets, logout ], function(err, res) {
+async.series([ login, getAllMarkets, getMarket, getMarketPrices, logout ], function(err, res) {
     process.exit(0);
 });
 
@@ -39,24 +39,114 @@ function keepAlive(callback) {
 }
 
 function getAllMarkets(cb) {
-    console.log('Get available tennis matches');
+	
+	function IsMatchingMarket(market) {
+		if (market.marketName == 'To Be Placed' || 
+			market.countryCode != 'GBR' ||
+			market.marketName == 'Reverse FC' ||
+			market.marketName == 'Forecast' ||
+			market.numberOfRunners < 4 ||
+			market.numberOfRunners > 20 ||
+			market.turningInPlay == 'Y')
+			return false;
+		else
+			return true;
+	}
+
+	console.log('Get available tennis matches');
 
     // eventTypeIds 1-soccer, 2-tennis, 7-todays card.
     var inv = session.getAllMarkets({
         eventTypeIds : [ events ]
     });
+
     inv.execute(function(err, res) {
-        console.log('action:', res.action, 'error:', err, 'duration:', res
-                .duration() / 1000);
-        if (err) {
+		//console.log(res);
+    	console.log(
+			'action:', res.action, 
+			', error:', err, 
+			', duration:', res.duration() / 1000);
+        
+		if (err) {
 			cb("Error in getAllMarkets", null);
-        }
-        for ( var index in res.result.marketData) {
-            market = res.result.marketData[index];
-            if (market.marketName != 'Match Odds')
+    	}
+
+    	for ( var index in res.result.marketData) {
+    		market = res.result.marketData[index];
+            
+			if (!IsMatchingMarket(market))
                 continue;
-            var path = market.menuPath;//.replace(/\\Tennis\\Group A\\/g, '');
-            console.log(market);
+				
+			console.log(market);
+        	
+			var path = market.menuPath;//.replace(/\\Tennis\\Group A\\/g, '');
+
+			console.log(path);
+    	}
+
+        cb(null, "OK");
+
+    });
+}
+
+function getMarket(cb) {
+    console.log('Call getMarket for marketId="%s"', events);
+    var inv = session.getMarket('105268904');
+    inv.execute(function(err, res) {
+        console.log('action:', res.action, 'error:', err, 'duration:',
+                res.duration() / 1000);
+        if (err) {
+            cb("Error in getMarket", null);
+        }
+        
+        console.log("marketId:", res.result.market.marketId);
+        console.log("market name:", res.result.market.name);
+        console.log("market time:", res.result.market.marketTime);
+
+		for(var index in res.result.market.runners) {
+        	//console.log("\tplayerOneId:", res.result.market.runners[index].selectionId);
+        	console.log("\tHorse Name:", res.result.market.runners[index].name);
+			console.log("\tHorse Object:", res.result.market.runners[index]);
+		}
+
+        cb(null, "OK");
+    });
+}
+
+var marketId = '105268904';
+function getMarketPrices(cb) {
+    console.log('Call getMarketPricesCompressed for marketId="%s"', marketId);
+    var inv = session.getMarketPricesCompressed(marketId);
+    inv.execute(function(err, res) {
+        console.log('action:', res.action, 'error:', err, 'duration:',
+                res.duration() / 1000);
+        if (err) {
+            cb("Error in getMarketPricesCompressed", null);
+        }
+        //console.log(util.inspect(res.result, false, 10));
+
+        var market = res.result.marketPrices;
+        console.log("marketId:", market.marketId);
+        console.log("currency:", market.currency);
+        console.log("marketStatus:", market.marketStatus);
+        console.log("inPlayDelay:", market.inPlayDelay);
+        // print players info
+        for ( var playerIndex = 0; playerIndex < market.runners.length; ++playerIndex) {
+            console.log("Horse %s", playerIndex);
+            var runner = market.runners[playerIndex];
+			console.log("\trunnerObject:", runner);
+            console.log("\tselectionId:", runner.selectionId);
+            console.log("\tlastPriceMatched:", runner.lastPriceMatched);
+            console.log("\ttotalMatched:", runner.totalMatched);
+            
+			/*for ( var cnt = 0; cnt < runner.backPrices.length; ++cnt) {
+                var item = runner.backPrices[cnt];
+                console.log("\t back price:%s amount:%s", item.price, item.amount);
+            }
+            for ( var cnt = 0; cnt < runner.layPrices.length; ++cnt) {
+                var item = runner.layPrices[cnt];
+                console.log("\t lay price:%s amount:%s", item.price, item.amount);
+            }*/
         }
         cb(null, "OK");
     });
