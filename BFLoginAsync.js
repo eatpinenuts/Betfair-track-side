a48bfe1b08e7f43e833a6666fba5dc1f02ed2ed5
eatpinenuts <b.flatman@gmail.com>
2012-07-12 07:52:07 +0200

    Update master

--- BFLoginAsync.js
+++ BFLoginAsync.js
@@ -10,7 +10,7 @@ var password = myArgs[1];
 var events = myArgs[2];
 var session = '';
 
-request('http://www.google.com', function (error, response, body) {
+request('http://odds.bestbetting.com/', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body) // Print the google web page.
   }
