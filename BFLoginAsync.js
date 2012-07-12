76a7c3b37713ed1a787ae414edeb9e163d4ace10
Billy Flatman <b.flatman@gmail.com>
2012-07-12 16:51:57 +0100

    pre merge

--- BFLoginAsync.js
+++ BFLoginAsync.js
@@ -1,5 +1,7 @@
 var betfair = require('betfair-sports-api');
 var async = require('async');
+var http = require('http'), url = require('url');
+
 
 var myArgs = process.argv.slice(2);
 console.log('myArgs: ', myArgs);
@@ -9,7 +11,7 @@ var password = myArgs[1];
 var events = myArgs[2];
 var session = '';
 
-async.series([ login, getAllMarkets, getMarket, getMarketPrices, logout ], function(err, res) {
+async.series([ login, getAllMarkets, getMarket, getMarketPrices, bookiesExtract, logout ], function(err, res) {
     process.exit(0);
 });
 
@@ -77,7 +79,7 @@ function getAllMarkets(cb) {
 			if (!IsMatchingMarket(market))
                 continue;
 				
-			console.log(market);
+			//console.log(market);
         	
 			var path = market.menuPath;//.replace(/\\Tennis\\Group A\\/g, '');
 
@@ -93,6 +95,7 @@ function getMarket(cb) {
     console.log('Call getMarket for marketId="%s"', events);
     var inv = session.getMarket('105268904');
     inv.execute(function(err, res) {
+
         console.log('action:', res.action, 'error:', err, 'duration:',
                 res.duration() / 1000);
         if (err) {
@@ -106,7 +109,7 @@ function getMarket(cb) {
 		for(var index in res.result.market.runners) {
         	//console.log("\tplayerOneId:", res.result.market.runners[index].selectionId);
         	console.log("\tHorse Name:", res.result.market.runners[index].name);
-			console.log("\tHorse Object:", res.result.market.runners[index]);
+			//console.log("\tHorse Object:", res.result.market.runners[index]);
 		}
 
         cb(null, "OK");
@@ -134,7 +137,7 @@ function getMarketPrices(cb) {
         for ( var playerIndex = 0; playerIndex < market.runners.length; ++playerIndex) {
             console.log("Horse %s", playerIndex);
             var runner = market.runners[playerIndex];
-			console.log("\trunnerObject:", runner);
+			//console.log("\trunnerObject:", runner);
             console.log("\tselectionId:", runner.selectionId);
             console.log("\tlastPriceMatched:", runner.lastPriceMatched);
             console.log("\ttotalMatched:", runner.totalMatched);
@@ -161,4 +164,54 @@ function logout(callback) {
             console.log('logout OK');
         callback(err,res);
     });
+}
+
+function bookiesExtract(callback) {
+	
+	console.log('bookiesExtract');
+
+	var options = {
+  		host: 'odds.bestbetting.com',
+  		port: 80,
+  		path: '/horse-racing/2012-07-12/newmarket/13-20/betting/',
+		'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6'
+	};
+
+	function request() {
+		http.get(options, function(response) {
+			// The page has moved make new request
+			if (response.statusCode === 302) {
+				var newLocation = url.parse(response.headers.location).host;
+				console.log('We have to make new request ' + newLocation);
+				options.host = newLocation;
+				options.path = '';
+				request();
+			} else {
+				console.log("Response: %d", response.statusCode);
+				response.on('data', function(chunk) {
+					console.log('Body ' + chunk);
+					callback(null, "OK");
+				});
+			}
+		}).on('error', function(err) {
+			console.log('Error %s', err.message);
+			callback(err.message, "Fail");
+		});
+	}
+
+	request();
+
+
+	
+	/*http.get(options, function(res) {
+  		console.log("Got response: " + res.statusCode);
+
+  		res.on("data", function(chunk) {
+    		console.log("BODY: " + chunk);
+			callback(null, "OK");
+  		});
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+		callback(e.message, "Fail");
+	});*/
 }
